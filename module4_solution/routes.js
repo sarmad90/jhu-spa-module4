f7c9b0e7df3ea82d839bc0105ef5a50e7da7741d
Sarmad Sabih <sam_sarmad@hotmail.com>
2016-10-26 02:06:38 +0500

    changes

--- module4_solution/routes.js
+++ module4_solution/routes.js
@@ -3,16 +3,21 @@
   RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
   function RoutesConfig($stateProvider, $urlRouterProvider) {
 
-    // Redirect to tab 1 if no other URL matches
-    $urlRouterProvider.otherwise('/tab1');
+    $urlRouterProvider.otherwise('/');
 
     // Set up UI states
     $stateProvider
       .state('tab1', {
         url: '/tab1',
-        // template: "<h2>Hi from routes</h2>"
-        templateUrl: 'src/tab1.html'
+        template: "<h2>Hi from routes</h2>"
+        // templateUrl: 'src/tab1.html'
       })
+
+      .state('home', {
+        url: '/',
+        template: "<h2>Home Page</h2>"
+      })
+
 
       .state('tab2', {
         url: '/tab2',
